8b9333f0cfecaccde20f93e00a6f9d40bf456fa9
Spencer Brereton <spencer.brereton@hotmail.com>
2026-03-09 02:52:11 -0600

    Max permissive CORS to debug access

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,11 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8081", "http://localhost:19006", "http://localhost:3000", "*"
-
+    origins "*"
     resource "*",
       headers: :any,
-      expose: ["Authorization"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ["Authorization"],
       credentials: false
   end
 end
