97207cc6ae7a0398274d6b83e57be0e4a690d0cf
Spencer Brereton <spencer.brereton@hotmail.com>
2026-03-09 02:50:35 -0600

    Standardize Puma for solo-worker production mode on Render

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -1,39 +1,15 @@
-# Puma can serve each request in a separate thread.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+# Puma configuration for Render
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads max_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# Render requires the app to listen on 0.0.0.0.
-#
+# Use the port Render tells us to use
 port ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` that Puma will run in.
-#
+# Use production environment
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directs Puma to preload the application before starting the workers.
-# This step is allowed to use data from the root of the application,
-# but it's important to be careful with any stateful connections like
-# database connections.
-#
-# preload_app!
+# Explicitly disable workers for now to avoid cluster-mode port collision issues
+workers 0
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
